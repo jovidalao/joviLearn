35f8cb9cba84f41b00a9707d32ba3a73152437c9
unknown <jovidalao@gmail.com>
2019-03-30 23:01:40 +0800

    add udp classs

--- javacode/netDemo/UDPRceiver.java
+++ javacode/netDemo/UDPRceiver.java
@@ -1,14 +1,36 @@
 package netDemo;
 
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class UDPRceiver{
     public static void main(String[] args) throws Exception{
         //先创建DatagramSocket对象，必须指定一个端口号
-        DatagramSocket ds = new DatagramSocket();
+        DatagramSocket ds = new DatagramSocket(9909);
+        while (true) {
 
-        //创建DatagramPacket对象
-        //储存发送的数据，
+            //创建DatagramPacket对象
+            byte[] ls = new byte[1024];
+
+            DatagramPacket dp = new DatagramPacket(ls, ls.length);
+            //储存发送的数据，
+            System.out.println("等待发送数据。");
+            ds.receive(dp);
+            System.out.println("已经成功接受数据");
+            //获取发送端的地址(从数据包中)
+            InetAddress ita = dp.getAddress();
+            System.out.println("发送端是" + ita.getHostAddress());
+
+            //接收真正的数据
+            byte[] data = dp.getData();
+            int len = dp.getLength();
+
+            String reveiveMsg = new String(data, 0, len);
+
+            System.out.println(reveiveMsg);
+        }
+        //ds.close();
     }
 
 }
--- javacode/netDemo/UDPSender.java
+++ javacode/netDemo/UDPSender.java
@@ -1,20 +1,24 @@
 package netDemo;
 
 import java.net.*;
+import java.util.Scanner;
 
 public class UDPSender {
     public static void main(String[] args) throws Exception {
         //新建一个收发器
         DatagramSocket ds = new DatagramSocket();
-        //新建一个数据包
-        byte[] by = "jovidalao".getBytes();
-        DatagramPacket dp = new DatagramPacket(by,by.length, InetAddress.getLocalHost(),12345);
-        //把数据包放入收发器
-        ds.send(dp);
-
-        System.out.println("done");
-
-        ds.close();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("输入要发送的数据。");
+            String msg = sc.nextLine();
+            byte[] by = msg.getBytes();
+            //新建一个数据包
+            DatagramPacket dp = new DatagramPacket(by, by.length, InetAddress.getLocalHost(), 9909);
+            //把数据包放入收发器
+            ds.send(dp);
+            System.out.println("done");
+        }
+        //ds.close();
     }
 
 }
